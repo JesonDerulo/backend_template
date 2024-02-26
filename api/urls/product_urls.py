0496d4aaed426a3aca396c378f47a9e8cd468a10
JesonDerulo <jasonderulo110015@gmail.com>
2024-02-26 09:51:57 -0800

    add product detail api

--- api/urls/product_urls.py
+++ api/urls/product_urls.py
@@ -3,4 +3,5 @@ from api.views import product_views as views
 
 urlpatterns = [
     path('', views.get_products, name='get-products'),
+    path('<str:pk>/', views.get_product, name='product-detail'),
 ]
--- api/views/product_views.py
+++ api/views/product_views.py
@@ -3,15 +3,33 @@ from rest_framework.response import Response
 from api.models import Product
 from rest_framework import status
 from api.serializers import ProductSerializer
+from django.core.exceptions import ObjectDoesNotExist
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_products(request):
     try:
         products = Product.objects.all()
         if not products:
-            return Response({'message': 'No products found'}, status=status.HTTP_204_NO_CONTENT)
+            return Response(
+                {"message": "No products found"}, status=status.HTTP_204_NO_CONTENT
+            )
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["GET"])
+def get_product(request, pk):
+
+    try:
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product, many=False)
+        return Response(serializer.data)
+    except ObjectDoesNotExist:
+        return Response(
+            {"message": "No product found!!!"}, status=status.HTTP_404_NOT_FOUND
+        )
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
